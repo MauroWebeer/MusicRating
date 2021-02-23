b7ba2cce612c7e21854f8281e97ec39c1f990740
MauroWebeer <maurowebeer@gmail.com>
2021-02-23 19:41:54 -0300

    criando genre

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_222848) do
+ActiveRecord::Schema.define(version: 2021_02_23_223823) do
+
+  create_table "musics", force: :cascade do |t|
+    t.string "name"
+    t.string "artist"
+    t.string "album"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
