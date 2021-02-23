6118e3f2bb40a6862ba2c05bcc960deeb7cf0e71
MauroWebeer <maurowebeer@gmail.com>
2021-02-23 20:13:13 -0300

    active admin

---
+++ app/admin/musics.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Music do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :name, :artist, :album
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :artist, :album]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   resources :ratings
   resources :genres
   resources :musics
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'musics#index'
+  
 end
